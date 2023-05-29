15415209193a837067341fdc3f528db52ea5079c
Doan Quang Tinh <quangtinh.hn@gmail.com>
2023-05-29 11:31:18 +0700

    Add morgan

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+app.use(morgan("combined"));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
