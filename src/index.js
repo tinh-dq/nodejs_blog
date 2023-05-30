4a15ac54cbdebed38b56c1e03b0fb04c26198dcb
Doan Quang Tinh <quangtinh.hn@gmail.com>
2023-05-30 14:57:33 +0700

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 // HTTP Logger
-const morgan = require("morgan");
-const create = require("express-handlebars").create;
-const hbs = create({ defaultLayout: "main", extname: ".hbs" });
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
+const morgan = require('morgan');
+const create = require('express-handlebars').create;
+const hbs = create({ defaultLayout: 'main', extname: '.hbs' });
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 // import route to express
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
 // HTTP Logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
 route(app);
