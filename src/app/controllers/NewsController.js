687bbd99190a48e6fff9d3c7df54e7edf10bac36
Doan Quang Tinh <quangtinh.hn@gmail.com>
2023-05-30 13:14:09 +0700

    Fix import to require for new version of hbs, add controller, route

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController {
+  // GET /news
+  index(req, res) {
+    res.render("news");
+  }
+  // GET /news/:id
+  show(req, res) {
+    res.send("news detail...");
+  }
+}
+
+module.exports = new NewsController();
